59a4666362f558fbc4124e986c229c6076db65f4
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-09-03 18:49:20 +0700

    Add concurency test option

--- concurency-test.js
+++ concurency-test.js
@@ -84,3 +84,19 @@ gettingUserName()
  */
 
 module.exports = { fetchingUserFromInternet, gettingUserName, NetworkError };
+
+function fetchUsername() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('JSUser');
+        }, 3000);
+    })
+}
+
+console.log("Fetching username...");
+fetchUsername().then((value) => {
+    console.log(`You are logged in as ${value}`);
+})
+    .finally(() => {
+        console.log("Welcome!");
+    })
