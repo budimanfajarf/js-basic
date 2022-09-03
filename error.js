c9c211b699b3053f22d09730c53facbd4eac3b02
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-09-03 17:32:56 +0700

    custom error

--- error.js
+++ error.js
@@ -1,20 +1,57 @@
+// let json = '{ "age": 30 }';
+
+// try {
+//     let user = JSON.parse(json);
+
+//     if (!user.name) {
+//         throw new SyntaxError("'name' is required.");
+//     }
+
+//     console.log(user.name);
+//     console.log(user.age);
+// } catch (error) {
+//     if (error instanceof SyntaxError) {
+//         console.log(`JSON Error: ${error.message}`);
+//     } else if (error instanceof ReferenceError) {
+//         console.log(error.message);
+//     } else {
+//         console.log(error.stack);
+//     }
+// }
+
+class ValidationError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "ValidationError";
+    }
+}
+
 let json = '{ "age": 30 }';
 
 try {
     let user = JSON.parse(json);
 
     if (!user.name) {
-        throw new SyntaxError("'name' is required.");
+        throw new ValidationError("'name' is required.");
+    }
+    if (!user.age) {
+        throw new ValidationError("'age' is required.");
     }
 
     console.log(user.name);
     console.log(user.age);
 } catch (error) {
     if (error instanceof SyntaxError) {
-        console.log(`JSON Error: ${error.message}`);
+        console.log(`JSON Syntax Error: ${error.message}`);
+    } else if (error instanceof ValidationError) {
+        console.log(`Invalid data: ${error.message}`);
     } else if (error instanceof ReferenceError) {
         console.log(error.message);
     } else {
         console.log(error.stack);
     }
 }
+
+/* output
+Invalid data: 'name' is required.
+*/
