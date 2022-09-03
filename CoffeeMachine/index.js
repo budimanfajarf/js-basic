a981527393b0a85c5ab55291442457f0a2f772d4
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-09-03 16:38:59 +0700

    Add state.js

--- CoffeeMachine/index.js
+++ CoffeeMachine/index.js
@@ -1,1 +1,13 @@
 console.log('test')
+
+const coffeeStock = require('./state');
+
+const makeCoffee = (type, miligrams) => {
+    if (coffeeStock[type] >= miligrams) {
+        console.log("Kopi berhasil dibuat!");
+    } else {
+        console.log("Biji kopi habis!");
+    }
+}
+
+makeCoffee("robusta", 80);
