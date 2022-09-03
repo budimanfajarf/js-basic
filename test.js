aab5fc53566494fa929c6d1f8394c869e32c0427
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-09-03 20:13:49 +0700

    final test

--- test.js
+++ test.js
@@ -146,3 +146,67 @@ console.log('myEagle', myEagle);
 //  module.exports = {
 //   Animal, Rabbit, Eagle, myRabbit, myEagle,
 // };
+
+// const oneTwoThree = [1, 2, 3];
+// const sevenEightNine = [7, 8, 9];
+
+// const result = [4, 5, 6, ...oneTwoThree, ...sevenEightNine];
+
+// console.log(result);
+
+// const artistsAndSongs = {
+//   "Keyakizaka46": ["Silent Majority"],
+//   "Blackpink": ["How You Like That", "Ice Cream"],
+//   "JKT48": ["Rapsodi", "Heavy Rotation"],
+//   "Twice": ["What is Love?"],
+// }
+
+// artistsAndSongs["Babymetal"] = ["Gimme chocolate"];
+
+// delete artistsAndSongs["Keyakizaka46"];
+
+// console.log(artistsAndSongs)
+
+// function multiply(num) {
+//   total = num * num;
+// }
+
+// const result = multiply(3);
+
+// console.log(result);
+
+// try {
+//   const arr = [1, 2, 3, 4];
+//   for (let i = 0; i <= 4; i++) {
+//     console.log(arr[i]);
+//   }
+// } catch (e) {
+//   console.log("Out of bounds");
+// }
+
+// function fetchUsername() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve('JSUser');
+//     }, 3000);
+//   })
+// }
+
+// console.log("Fetching username...");
+// const username = fetchUsername();
+// console.log(`You are logged in as ${username}`);
+// console.log("Welcome!");
+
+function fetchUsername() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('JSUser');
+    }, 3000);
+  })
+}
+
+console.log("Fetching username...");
+fetchUsername().then((value) => {
+  console.log(`You are logged in as ${value}`);
+})
+console.log("Welcome!");
